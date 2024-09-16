ceacf7c8a4cb04ddf83ed0608525b55bc93b3185
Sami Rollins <srollins@gmail.com>
2024-09-16 11:18:03 -0700

    functions practice assignment

---
+++ lucky_sum.py
@@ -0,0 +1,20 @@
+"""
+Write a Python program that implements a function 
+lucky_sum(a: int, b:int , c:int) -> int 
+that takes as input three integers and returns their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+
+Implement a main method that tests this function by calling it at least five
+times with different test cases. For full credit, make sure your test cases test
+different possible conditions.
+
+This exercise is taken from the coding bat website. Students may not  
+"""
+
+def lucky_sum(a: int, b:int , c:int) -> int:
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ rps.py
@@ -0,0 +1,23 @@
+"""
+Write a Python program that uses functions to play the game rock, paper, scissors. Play will proceed as follows:
+
+- A random number will be selected to represent the computer choice. A 1 will be "rock"; a 2 will be "paper"; and a 3 will be "scissors".
+- The user will be prompted to enter a choice of rock, paper, or scissors.
+- If the user choice is valid the computer choice and user choice will be compared and a winner will be declared.
+  * paper beats rock 
+  * rock beats scissors
+  * scissors beats paper
+- If the user choice is not valid then play will end.
+
+Hints:
+- Grading will primarily depend upon how you decompose your program into
+functions. Consider the steps listed above as you think about which functions to
+implement. Also, consider going back to the preliminary design we discussed in class.
+- You must validate the user input to confirm that it is either rock, paper, or scissors.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
